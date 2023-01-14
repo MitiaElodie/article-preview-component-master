54c2f1239a72393910a20d70aa3802f84441be08
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-01-14 14:52:55 -0500

    add the html body

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,29 @@
 <template>
-  <div>
-      Shift the overall look and feel by adding these wonderful 
-  touches to furniture in your home
-
-  Ever been in a room and felt like something was missing? Perhaps 
-  it felt slightly bare and uninviting. I’ve got some simple tips 
-  to help you make any room feel complete.
-
-  Michelle Appleton
-  28 Jun 2020
-
-  Share
-  
-  <div class="attribution">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-    Coded by <a href="#">Your Name Here</a>.
+  <div class="article">
+    <div class="article__container">
+      <div class="article__img-container"></div>
+      <div class="article__details-container">
+        <h1>Shift the overall look and feel by adding these wonderful touches to furniture in your home</h1>
+        <p>Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
+        <div class="article__author-container">
+          <img class="article__author-img" src="./assets/avatar-michelle.jpg" alt="Headshot of Michelle Appleton" />
+          <div class="article__author-details">
+            <p>Michelle Appleton</p>
+            <p>28 Jun 2020</p>
+          </div>
+          <div class="article__send-btn">
+            <img class="article__send-icon" src="./assets/icon-share.svg" alt="Send icon" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  </div>
+  <footer>
+    <div class="attribution">
+      Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+      Coded by <a href="#">Your Name Here</a>.
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -32,15 +39,7 @@ $grayish-blue: hsl(212, 23%, 69%);
 $light-grayish-blue: hsl(210, 46%, 95%);
 // font-size: 13px
 // font-family: https://fonts.google.com/specimen/Manrope 500, 700
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-    .attribution { font-size: 11px; text-align: center; }
-    .attribution a { color: hsl(228, 45%, 44%); }
+.attribution { font-size: 11px; text-align: center; }
+.attribution a { color: hsl(228, 45%, 44%); }
 </style>
