a01657a9cbc64c79727594188022c14ba2da1739
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-01-14 14:31:41 -0500

    init vue project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+      Shift the overall look and feel by adding these wonderful 
+  touches to furniture in your home
+
+  Ever been in a room and felt like something was missing? Perhaps 
+  it felt slightly bare and uninviting. I’ve got some simple tips 
+  to help you make any room feel complete.
+
+  Michelle Appleton
+  28 Jun 2020
+
+  Share
+  
+  <div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+$very-dark-grayish-blue: hsl(217, 19%, 35%);
+$desaturated-dark-blue: hsl(214, 17%, 51%);
+$grayish-blue: hsl(212, 23%, 69%);
+$light-grayish-blue: hsl(210, 46%, 95%);
+// font-size: 13px
+// font-family: https://fonts.google.com/specimen/Manrope 500, 700
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+    .attribution { font-size: 11px; text-align: center; }
+    .attribution a { color: hsl(228, 45%, 44%); }
+</style>
