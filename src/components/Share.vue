abca545015b10dc1fecc736ab5876246ae92f228
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-03-25 17:49:45 -0400

    create the share component

---
+++ src/components/Share.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="share__container">
+    <span class="share__text">SHARE</span>
+    <img class="share__icon" src="@/assets/icon-facebook.svg" alt="Facebook icon" />
+    <img class="share__icon" src="@/assets/icon-twitter.svg" alt="Twitter icon" />
+    <img class="share__icon" src="@/assets/icon-pinterest.svg" alt="Pinterest icon" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Share',
+}
+</script>
