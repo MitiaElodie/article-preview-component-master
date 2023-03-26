975f49ee913e97784042c2a18531408e4fc747f1
Anjara Mitia Elodie <elodie.mitia@conformit.com>
2023-03-26 18:56:11 -0400

    install vue-tippy

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueTippy from 'vue-tippy'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(VueTippy, {
+  defaultProps: { placement: 'right' },
+})
+
+app.mount('#app')
